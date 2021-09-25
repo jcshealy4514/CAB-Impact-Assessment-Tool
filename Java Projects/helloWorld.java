6e54134af9400acfb91f49a438faf303a6cdc70d
jcshealy4514 <65636255+jcshealy4514@users.noreply.github.com>
2021-09-25 18:00:28 -0400

    Remove PauseScript Variable
    Remove PauseScript Variable because it's not needed to pause the script. Perhaps there is a better solution to pause the script?

--- Java Projects/helloWorld.java
+++ Java Projects/helloWorld.java
@@ -5,12 +5,11 @@ public class helloWorld {
     public static void main(String args[]){
 
         String UserInput;
-        String PauseScript;
         System.out.println("Hello World!");
         Scanner scanner = new Scanner(System.in);
         UserInput = scanner.nextLine();
         System.out.println(UserInput);
-        PauseScript = scanner.nextLine();
+        scanner.nextLine();
         scanner.close();
 
     }
